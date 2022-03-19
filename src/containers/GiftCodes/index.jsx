a037ee09bf809657ed133a459a9babb420617a33
panda1114Git <panda.kk1114@gmail.com>
2022-03-19 02:30:15 +0200

    Add GiftCodes page

--- src/containers/GiftCodes/index.jsx
+++ src/containers/GiftCodes/index.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Col, Container, Row } from 'reactstrap';
-const GiftCodes = () => {
+import TabNavs from '../../shared/components/tabNavs';
+import Sidebar from '../Layout/sidebar';
+import GiftCard from './components/GiftCard';
+import giftData from './components/Mockdata.json';
+
+const IMG_BACK = `${process.env.PUBLIC_URL}/img/pages/my-gift-codes.jpg`;
+
+const MyGiftCodes = () => {
   return (
-    <Container className="dashboard">
-      GiftCodes
-    </Container>
+    <>
+      <div className='mainboard'>
+        <img src={IMG_BACK}/>
+        <div className='title text-dark'>
+          My Gift Codes
+        </div>
+      </div>
+      <TabNavs />
+      <Container className='mb-5'>
+        <Row>
+          <Col md={9}>
+          <div className="dashboard">
+           {giftData.map((item, index)=>{
+             return <GiftCard key={index} data={item}/>
+           })}   
+              
+          </div>
+          </Col>
+          <Col md={3}>
+              <Sidebar/>
+          </Col>
+        </Row>
+      </Container>
+    </>
 )}
 
-export default GiftCodes;
+export default MyGiftCodes;
