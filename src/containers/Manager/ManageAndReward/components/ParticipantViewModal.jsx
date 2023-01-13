02608ca545f9dae27e3415175e4fb4414c0a5c8f
Amol Kaur <amol@inimisttech.com>
2023-01-13 15:54:02 +0530

    fixes

--- src/containers/Manager/ManageAndReward/components/ParticipantViewModal.jsx
+++ src/containers/Manager/ManageAndReward/components/ParticipantViewModal.jsx
@@ -196,70 +196,70 @@ const ParticipantViewModal = ({
                 <Table striped bordered hover>
                     <thead>
                         <tr>
-                            <td colspan="8" class="title">
+                            <td colSpan="8" className="title">
                                 Reward History
                             </td>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <th class="title-col"></th>
-                            <th class="title-col"> Event </th>
-                            <th class="title-col">Notes</th>
-                            <th class="title-col"> Referrer </th>
-                            <th class="title-col value">Date</th>
-                            <th class="title-col value">Reference Document</th>
-                            <th class="value">Points</th>
+                            <th className="title-col"></th>
+                            <th className="title-col"> Event </th>
+                            <th className="title-col">Notes</th>
+                            <th className="title-col"> Referrer </th>
+                            <th className="title-col value">Date</th>
+                            <th className="title-col value">Reference Document</th>
+                            <th className="value">Points</th>
                         </tr>
-                        <tr class="odd">
-                            <td class="title-col"></td>
-                            <td class="title-col"> Reclaim points </td>
-                            <td class="title-col"> audit cleanup test awards </td>
-                            <td class="title-col"></td>
-                            <td class="title-col value"> 12/31/2022 </td>
-                            <td class="title-col" id="pdf-"></td>
-                            <td class="title-col value"> -400 </td>
+                        <tr className="odd">
+                            <td className="title-col"></td>
+                            <td className="title-col"> Reclaim points </td>
+                            <td className="title-col"> audit cleanup test awards </td>
+                            <td className="title-col"></td>
+                            <td className="title-col value"> 12/31/2022 </td>
+                            <td className="title-col" id="pdf-"></td>
+                            <td className="title-col value"> -400 </td>
                             <td></td>
                         </tr>
-                        <tr class="even">
-                            <td class="title-col"></td>
-                            <td class="title-col"> Reclaim points </td>
-                            <td class="title-col"> audit cleanup test awards </td>
-                            <td class="title-col"></td>
-                            <td class="title-col value"> 12/31/2022 </td>
-                            <td class="title-col" id="pdf-"></td>
-                            <td class="title-col value"> -200 </td>
+                        <tr className="even">
+                            <td className="title-col"></td>
+                            <td className="title-col"> Reclaim points </td>
+                            <td className="title-col"> audit cleanup test awards </td>
+                            <td className="title-col"></td>
+                            <td className="title-col value"> 12/31/2022 </td>
+                            <td className="title-col" id="pdf-"></td>
+                            <td className="title-col value"> -200 </td>
                             <td></td>
                         </tr>
-                        <tr class="odd">
-                            <td class="title-col">
-                                <div class="event-icon g"></div>
+                        <tr className="odd">
+                            <td className="title-col">
+                                <div className="event-icon g"></div>
                             </td>
-                            <td class="title-col"> Happy Birthday! </td>
-                            <td class="title-col"></td>
-                            <td class="title-col"></td>
-                            <td class="title-col value"> 08/12/2022 </td>
-                            <td class="title-col" id="pdf-465656"></td>
-                            <td class="title-col value"> 0 </td>
+                            <td className="title-col"> Happy Birthday! </td>
+                            <td className="title-col"></td>
+                            <td className="title-col"></td>
+                            <td className="title-col value"> 08/12/2022 </td>
+                            <td className="title-col" id="pdf-465656"></td>
+                            <td className="title-col value"> 0 </td>
                             <td>
                                 <a href="/manager/participant/print-award-email/404628/465656" target="_blank" title="Print">
                                     <img src="/assets/img/printer.png" />
                                 </a>
-                                <a href="" onclick="Participant.getInstance().resendAward(404628 , 465656); return false;" title="Resend">
+                                <a href="" title="Resend">
                                     <img src="/assets/img/mail.png" />
                                 </a>
                             </td>
                         </tr>
-                        <tr class="even">
-                            <td class="title-col">
-                                <div class="event-icon g"></div>
+                        <tr className="even">
+                            <td className="title-col">
+                                <div className="event-icon g"></div>
                             </td>
-                            <td class="title-col"> Award </td>
-                            <td class="title-col"></td>
-                            <td class="title-col"></td>
-                            <td class="title-col value"> 04/14/2022 </td>
-                            <td class="title-col" id="pdf-449264"></td>
-                            <td class="title-col value"> 400 </td>
+                            <td className="title-col"> Award </td>
+                            <td className="title-col"></td>
+                            <td className="title-col"></td>
+                            <td className="title-col value"> 04/14/2022 </td>
+                            <td className="title-col" id="pdf-449264"></td>
+                            <td className="title-col value"> 400 </td>
                             <td>
                                 <a href="/manager/participant/print-award-email/404628/449264" target="_blank" title="Print">
                                     <img src="/assets/img/printer.png" />
@@ -269,16 +269,16 @@ const ParticipantViewModal = ({
                                 </a>
                             </td>
                         </tr>
-                        <tr class="odd">
-                            <td class="title-col">
-                                <div class="event-icon i"></div>
+                        <tr className="odd">
+                            <td className="title-col">
+                                <div className="event-icon i"></div>
                             </td>
-                            <td class="title-col"> Helping Hand </td>
-                            <td class="title-col"> Peer 2 Peer Award </td>
-                            <td class="title-col"></td>
-                            <td class="title-col value"> 04/06/2022 </td>
-                            <td class="title-col" id="pdf-448261"></td>
-                            <td class="title-col value"> 0 </td>
+                            <td className="title-col"> Helping Hand </td>
+                            <td className="title-col"> Peer 2 Peer Award </td>
+                            <td className="title-col"></td>
+                            <td className="title-col value"> 04/06/2022 </td>
+                            <td className="title-col" id="pdf-448261"></td>
+                            <td className="title-col value"> 0 </td>
                             <td>
                                 <a href="/manager/participant/print-award-email/404628/448261" target="_blank" title="Print">
                                     <img src="/assets/img/printer.png" />
@@ -288,16 +288,16 @@ const ParticipantViewModal = ({
                                 </a>
                             </td>
                         </tr>
-                        <tr class="even">
-                            <td class="title-col">
-                                <div class="event-icon i" />
+                        <tr className="even">
+                            <td className="title-col">
+                                <div className="event-icon i" />
                             </td>
-                            <td class="title-col"> Helping Hand </td>
-                            <td class="title-col"> Peer 2 Peer Award </td>
-                            <td class="title-col"></td>
-                            <td class="title-col value"> 03/03/2022 </td>
-                            <td class="title-col" id="pdf-444680"></td>
-                            <td class="title-col value"> 0 </td>
+                            <td className="title-col"> Helping Hand </td>
+                            <td className="title-col"> Peer 2 Peer Award </td>
+                            <td className="title-col"></td>
+                            <td className="title-col value"> 03/03/2022 </td>
+                            <td className="title-col" id="pdf-444680"></td>
+                            <td className="title-col value"> 0 </td>
                             <td>
                                 <a href="/manager/participant/print-award-email/404628/444680" target="_blank" title="Print">
                                     <img src="/assets/img/printer.png" />
@@ -307,16 +307,16 @@ const ParticipantViewModal = ({
                                 </a>
                             </td>
                         </tr>
-                        <tr class="odd">
-                            <td class="title-col">
-                                <div class="event-icon g"></div>
+                        <tr className="odd">
+                            <td className="title-col">
+                                <div className="event-icon g"></div>
                             </td>
-                            <td class="title-col"> Peer Award Limited </td>
-                            <td class="title-col"> Peer 2 Peer Award </td>
-                            <td class="title-col"></td>
-                            <td class="title-col value"> 03/03/2022 </td>
-                            <td class="title-col" id="pdf-444679"></td>
-                            <td class="title-col value"> 200 </td>
+                            <td className="title-col"> Peer Award Limited </td>
+                            <td className="title-col"> Peer 2 Peer Award </td>
+                            <td className="title-col"></td>
+                            <td className="title-col value"> 03/03/2022 </td>
+                            <td className="title-col" id="pdf-444679"></td>
+                            <td className="title-col value"> 200 </td>
                             <td>
                                 <a href="/manager/participant/print-award-email/404628/444679" target="_blank" title="Print">
                                     <img src="/assets/img/printer.png" />
@@ -326,36 +326,36 @@ const ParticipantViewModal = ({
                                 </a>
                             </td>
                         </tr>
-                        <tr class="even">
-                            <td class="title-col"></td>
-                            <td class="title-col"> Expire points </td>
-                            <td class="title-col"></td>
-                            <td class="title-col"></td>
-                            <td class="title-col value"> 01/06/2022 </td>
-                            <td class="title-col" id="pdf-"></td>
-                            <td class="title-col value"> -40 </td>
+                        <tr className="even">
+                            <td className="title-col"></td>
+                            <td className="title-col"> Expire points </td>
+                            <td className="title-col"></td>
+                            <td className="title-col"></td>
+                            <td className="title-col value"> 01/06/2022 </td>
+                            <td className="title-col" id="pdf-"></td>
+                            <td className="title-col value"> -40 </td>
                             <td></td>
                         </tr>
-                        <tr class="odd">
-                            <td class="title-col"></td>
-                            <td class="title-col"> Expire points </td>
-                            <td class="title-col"></td>
-                            <td class="title-col"></td>
-                            <td class="title-col value"> 12/31/2021 </td>
-                            <td class="title-col" id="pdf-"></td>
-                            <td class="title-col value"> -3,960 </td>
+                        <tr className="odd">
+                            <td className="title-col"></td>
+                            <td className="title-col"> Expire points </td>
+                            <td className="title-col"></td>
+                            <td className="title-col"></td>
+                            <td className="title-col value"> 12/31/2021 </td>
+                            <td className="title-col" id="pdf-"></td>
+                            <td className="title-col value"> -3,960 </td>
                             <td></td>
                         </tr>
-                        <tr class="even">
-                            <td class="title-col">
-                                <div class="event-icon g"></div>
+                        <tr className="even">
+                            <td className="title-col">
+                                <div className="event-icon g"></div>
                             </td>
-                            <td class="title-col"> Happy Birthday! </td>
-                            <td class="title-col"></td>
-                            <td class="title-col"></td>
-                            <td class="title-col value"> 08/12/2021 </td>
-                            <td class="title-col" id="pdf-407214"></td>
-                            <td class="title-col value"> 0 </td>
+                            <td className="title-col"> Happy Birthday! </td>
+                            <td className="title-col"></td>
+                            <td className="title-col"></td>
+                            <td className="title-col value"> 08/12/2021 </td>
+                            <td className="title-col" id="pdf-407214"></td>
+                            <td className="title-col value"> 0 </td>
                             <td>
                                 <a href="/manager/participant/print-award-email/404628/407214" target="_blank" title="Print">
                                     <img src="/assets/img/printer.png" />
