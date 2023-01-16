71abcb099036301c7992f4ecef4c58fd8df2ccf5
olaccursed <olaccursed@yandex.ru>
2023-01-16 04:28:06 +0300

    V3F-77 tmp..

--- src/shared/components/socialWall/SocialWallPanel.jsx
+++ src/shared/components/socialWall/SocialWallPanel.jsx
@@ -9,8 +9,13 @@ import { useDispatch } from "react-redux";
 import { Button, Col, Container, Row } from "reactstrap";
 import TemplateButton from "@/shared/components/TemplateButton";
 import { useTranslation } from "react-i18next";
+import SlideOutMenu from "../../../containers/Participant/Home/components/slide-out-menu";
+import PointsOrigin from "../../../containers/Layout/sidebar/PointsOrigin";
+import {ParticipantTabNavs, ParticipantTabNavsOrigin} from "../tabNavs";
+import {Slider, SliderOrigin} from "../../../containers/Participant/Home/components/slider";
+import {Sidebar} from "../../../containers/Layout/sidebar";
 
-const SocialWallPanel = ({ auth, organization, program, isManager }) => {
+const SocialWallPanel = ({ auth, organization, program, isManager, template }) => {
   const { t } = useTranslation();
   let [socialWallPosts, setSocialWallPosts] = useState(null);
   let [socialWallPost, setSocialWallPost] = useState(null);
@@ -57,15 +62,50 @@ const SocialWallPanel = ({ auth, organization, program, isManager }) => {
     }
   }, [organization, program]);
 
-  if (!socialWallPosts) return t("loading");
+  if (!socialWallPosts || !template) return t("loading");
 
-  return (
-    <Container>
-      <Row>
+  const SocialWallOrigin = () => {
+    return (
+      <>
         {isManager && (
           <TemplateButton className="add-new-post" text={t("add_new_post")} />
         )}
-        <div className="panel social-wall-panel pt-4">
+        <div className={`social-wall-panel-${template.name}`} >
+          {socialWallPosts.results.map((item, index) => {
+            return (
+              <div key={`socialWallItem-${index}`}>
+                <SocialWallItem
+                  program={program}
+                  data={item}
+                  popupToggle={popupToggle}
+                  confirmRef={confirmRef}
+                  setSocialWallPost={setSocialWallPost}
+                  isManager={isManager}
+                  setDeleteActivityId={setDeleteActivityId}
+                />
+                <div>&nbsp;</div>
+              </div>
+            );
+          })}
+        </div>
+        <SocialWallCommentModalWrapper
+          isOpen={isOpen}
+          setOpen={setOpen}
+          toggle={popupToggle}
+          socialWallPost={socialWallPost}
+          setSocialWallPosts={setSocialWallPosts}
+        />
+        <ConfirmModalWrapper confirmRef={confirmRef} action={deleteActivity} />
+      </>
+    );
+  };
+  const SocialWallNew = () => {
+    return (
+      <>
+        {isManager && (
+          <TemplateButton className="add-new-post" text={t("add_new_post")} />
+        )}
+        <div className={`social-wall-panel-${template.name} panel pt-4`} >
           {socialWallPosts.results.map((item, index) => {
             return (
               <div key={`socialWallItem-${index}`}>
@@ -83,16 +123,21 @@ const SocialWallPanel = ({ auth, organization, program, isManager }) => {
             );
           })}
         </div>
-      </Row>
-      <SocialWallCommentModalWrapper
-        isOpen={isOpen}
-        setOpen={setOpen}
-        toggle={popupToggle}
-        socialWallPost={socialWallPost}
-        setSocialWallPosts={setSocialWallPosts}
-      />
-      <ConfirmModalWrapper confirmRef={confirmRef} action={deleteActivity} />
-    </Container>
+        <SocialWallCommentModalWrapper
+          isOpen={isOpen}
+          setOpen={setOpen}
+          toggle={popupToggle}
+          socialWallPost={socialWallPost}
+          setSocialWallPosts={setSocialWallPosts}
+        />
+        <ConfirmModalWrapper confirmRef={confirmRef} action={deleteActivity} />
+      </>
+    );
+  };
+
+  return (
+    (template?.name === "Original" && <SocialWallOrigin />) ||
+    (template?.name === "New" && <SocialWallNew />)
   );
 };
 
@@ -101,6 +146,7 @@ const mapStateToProps = (state) => {
     auth: state.auth,
     program: state.program,
     organization: state.organization,
+    template: state.template,
   };
 };
 export default connect(mapStateToProps)(SocialWallPanel);
