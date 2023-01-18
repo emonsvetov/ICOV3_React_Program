035ac1df92fb4869b508add294d8f8bde17f76d6
jaymoorejmcc <jaygdmoore@gmail.com>
2023-01-17 19:38:07 -0500

    update text
    update text

--- src/containers/Manager/ManageAndReward/index.jsx
+++ src/containers/Manager/ManageAndReward/index.jsx
@@ -25,7 +25,7 @@ const ManageAndReward = ({auth, program, organization}) => {
           </div>
           <div className='d-flex'>
             <SearchIcon size={36} className='icon'/>
-            <span>Search in all Programs</span>
+            <span>Search Program</span>
           </div>
         </div>
         {auth && program && !isEmpty(organization) && <ProgramParticipants organization={organization} program={program} />}
