3f43c677ae644565e1180e3aa91941cd6828c2ba
Arvind Kumar <arvind@inimisttech.com>
2023-02-14 21:19:22 +0530

    peer2peer awarding fix

--- src/shared/components/flash/ApiErrorMessage.jsx
+++ src/shared/components/flash/ApiErrorMessage.jsx
@@ -1,7 +1,16 @@
 const ApiErrorMessage = ({errors, showLabel = true, className}) => {
+  console.log(errors)
     className = 'apiError-wrap form-error mt-0' + (className ? ' ' + className : '')
     if(!errors?.errors){
+      if(errors?.message)
+      {
         errors.errors = errors.message; //sometimes there is not errors but just a message!
+      } else {
+        errors.errors = "There was an error"
+      }
+    }
+    if(errors?.e){
+      errors.errors = errors.e; //sometimes there is "e" as well!
     }
     return (
         <div className={className}>
