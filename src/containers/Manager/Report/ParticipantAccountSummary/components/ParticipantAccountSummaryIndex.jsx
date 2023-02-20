ebfc9941890c89e41a87bbe25b8eface60c9f57f
olaccursed <olaccursed@yandex.ru>
2023-02-21 02:21:29 +0300

    ICO-150. Participant Account Summary Report - Done.

---
+++ src/containers/Manager/Report/ParticipantAccountSummary/components/ParticipantAccountSummaryIndex.jsx
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import { Card, CardBody, Col } from 'reactstrap';
+import ParticipantAccountSummaryTable from './ParticipantAccountSummaryTable';
+import axios from "axios";
+import {isEmpty} from '@/shared/helpers'
+import {connect} from "react-redux";
+import ParticipantAccountSummary from "../index";
+
+const ParticipantAccountSummaryIndex = ({ organization }) => {
+  const [programs, setPrograms] = useState([]);
+  const [defaultPrograms, setDefaultPrograms] = useState([]);
+
+
+  const getData = async () => {
+    const programsApiUrl = `/organization/${organization.id}/program?page=0&limit=9999999999&hierarchy=1&all=1`
+    if (isEmpty(programs)) {
+      try {
+        const response = await axios.get(programsApiUrl);
+        if (response.data.length === 0) return {results: [], count: 0}
+
+        const data = response.data.data;
+        setPrograms(data);
+        return data;
+      } catch (e) {
+        throw new Error(`API error:${e?.message}`);
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (organization) {
+      getData();
+    }
+    if (programs) {
+      const result = programs.map(x => x.account_holder_id)
+      setDefaultPrograms(result);
+    }
+  }, [programs, organization])
+
+  if (isEmpty(defaultPrograms)) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <Col md={12}>
+      <Card>
+        <CardBody>
+          <ParticipantAccountSummaryTable programs={defaultPrograms} />
+        </CardBody>
+      </Card>
+    </Col>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    organization: state.organization,
+  };
+};
+export default connect(mapStateToProps)(ParticipantAccountSummaryIndex);
