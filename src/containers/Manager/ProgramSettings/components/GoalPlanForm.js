985726ae2937489c4c4b9a7e99f8ae259d0ccdfe
Amol Kaur <amol@inimisttech.com>
2023-02-23 14:56:19 +0530

    goal plan modal reload

--- src/containers/Manager/ProgramSettings/components/GoalPlanForm.js
+++ src/containers/Manager/ProgramSettings/components/GoalPlanForm.js
@@ -390,8 +390,6 @@ const GoalPlanForm = ({
                 dispatch(sendFlashMessage(msg));
                 setLoading(false);
                 setOpen(false);
-                
-            // toggle()
               }
           })
           .catch((err) => {
