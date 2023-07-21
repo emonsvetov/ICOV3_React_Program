a0689fd6da8f00ef2d193afc773bd70627f0ce6a
Ashwani Kumar <ashwanikumar@inimist.com>
2023-07-21 17:16:48 +0530

    add a component manage-account

--- src/containers/Layout/topbar/Manager.jsx
+++ src/containers/Layout/topbar/Manager.jsx
@@ -20,6 +20,7 @@ const LINKS = [
   { to: "/manager/report/supplier-redemption", text: "View Reports" },
   { to: "/manager/invite-participant", text: "Invite New Participant" },
   { to: "/manager/referral", text: "Referral Administrator" },
+  { to: "/manager/manage-account", text: "Manage Account" },
   { to: "/manager/team", text: "Team" },
 ];
 
