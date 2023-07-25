6f6ba7f32ec4ebb888baf8bd016d0c489e99c59c
Ashwani Kumar <ashwanikumar@inimist.com>
2023-07-25 12:54:44 +0530

    work multiple invoice

--- src/containers/Manager/ManageAccount/components/MultipleInvoicesForm.jsx
+++ src/containers/Manager/ManageAccount/components/MultipleInvoicesForm.jsx
@@ -1,34 +1,54 @@
 import { Input, Col, Row, FormGroup, Label, Button } from "reactstrap";
 import { Form, Field } from "react-final-form";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-// import renderToggleButtonField from "@/shared/components/form/ToggleButton"
-// import { BuildProgramOptions } from '@/shared/helpers';
+import { AUTH_SELECT_PROGRAM_TREE } from "@/containers/App/auth";
 import {
   useDispatch,
   flashError,
   flashSuccess,
 } from "@/shared/components/flash";
-// import formValidation from "@/validation/addReferralNotificationRecipient"
+
 import TemplateButton from "@/shared/components/TemplateButton";
-// import { getProgram } from "@/services/program/getProgram";
+import { getProgramTree } from "@/services/program/getProgramTree";
 
 
-// const getCachedProgramTree = (tree) => {
-//   return localStorage.getItem(AUTH_SELECT_PROGRAM_TREE);
-// }
+
+const getCachedProgramTree = (tree) => {
+  return localStorage.getItem(AUTH_SELECT_PROGRAM_TREE);
+}
+
+
 
 const MultipleInvoiceForm = ({
- 
+  rootProgram,
   toggle,
+  auth,
   program,
   btnLabel = "Create Multipe Invoices",
 }) => {
+  
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
-  
+  const [options, setOptions] = useState([]);
+  // console.log('AA gya data',options);
+
+  useEffect(() => {
+    const cachedTree = getCachedProgramTree()
+    if( cachedTree ) {
+      setOptions(JSON.parse(cachedTree));
+    } else {
+      if (rootProgram && rootProgram?.id) {
+        getProgramTree(auth.organization_id, rootProgram.id).then((p) => {
+         
+          setOptions(p);
+        });
+      }
+    }
+  }, [rootProgram]);
   const onSubmit = (values) => {
+    console.log(values, "done");
     // // console.log('edit')
     let formData = {};
     formData.amount = values?.amount;
@@ -55,71 +75,29 @@ const MultipleInvoiceForm = ({
       });
   };
 
-  const BuildProgramOptions = ({programs, depth = 0}) => {
-    let optionsHtml = []
-    if( programs.length > 0) {
-        programs.map( p => {
-            optionsHtml.push(<option key={`program-option-${p.id}`} value={`${p.id}`}>{'-'.repeat(depth)} {p.name}</option>)
-            if( p?.children && p.children.length > 0)   {
-                depth++;
-                optionsHtml.push(<BuildProgramOptions key={`program-option-group-${p.id}`} programs={p.children} depth={depth} />)
-            }
-        })
-    }
-    return optionsHtml
-}
-
-
-
- 
   return (
     <Form
       onSubmit={onSubmit}
       // validate={(values) => formValidation.validateForm(values)}
-      
     >
       {({ handleSubmit, form, submitting, pristine, values }) => (
         <form
           className="form d-flex flex-column justify-content-evenly"
           onSubmit={handleSubmit}
         >
-          <Input
-          type="select"
-          value={program.id}
-          name="program"
-          id="program-select"
+          <Row>
+                <Col md="4">Program</Col>
           
-        >
-          <BuildProgramOptions />
-        </Input>
-          <Row>
-            <Col md="4">
-              <b>Program</b>
-            </Col>
-            <Col md="4">
-              <b>*Amount</b>
-            </Col>
-            <Col md="4">
-              <b>*Confirm Amount</b>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col>:</Col>
-            <Col md="4">
-              <Field name="amount">
-                {({ input, meta }) => (
-                  <FormGroup>
-                    <Input placeholder="$" type="text" {...input} />
-                    {meta.touched && meta.error && (
-                      <span className="text-danger">{meta.error}</span>
-                    )}
-                  </FormGroup>
-                )}
-              </Field>
-            </Col>
-            <Col md="4">
-              <Field name="amount_confirmation">
+                <Col md="4">Amount</Col>
+            
+                <Col md="4">Confirm Amount</Col>
+              </Row>
+          {options.map((program) => (
+            <div>
+              <Row>
+                <Col md="4">{program.name}</Col>
+                <Col md="4">
+                <Field name="amount">
                 {({ input, meta }) => (
                   <FormGroup>
                     <Input
@@ -132,9 +110,25 @@ const MultipleInvoiceForm = ({
                     )}
                   </FormGroup>
                 )}
-              </Field>
-            </Col>
-          </Row>
+              </Field></Col>
+              <Col md="4">
+                <Field name="amount_confirmation">
+                {({ input, meta }) => (
+                  <FormGroup>
+                    <Input
+                      placeholder="$"
+                      type="text"
+                      {...input}
+                    />
+                    {meta.touched && meta.error && (
+                      <span className="text-danger">{meta.error}</span>
+                    )}
+                  </FormGroup>
+                )}
+              </Field></Col>
+              </Row>
+            </div>
+          ))}
           <div className="d-flex justify-content-end">
             <TemplateButton disabled={loading} type="submit" text={btnLabel} />
           </div>
