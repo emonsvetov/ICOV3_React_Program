9a68f1174c20c60fd741f638d654cf0b059fbe98
Arvind Kumar <arvind.mailto@gmail.com>
2023-08-17 11:49:59 +0530

    fixing message

---
+++ src/shared/components/tabNavs/components/TabnavItems.jsx
@@ -0,0 +1,38 @@
+
+
+export const getTabnavItems = (program) => {
+
+  const TAB_ITEMS = [
+    // { title: "my_rewards", icon: "MyRewards", to: `/participant/my-points` },
+  ];
+
+  if( !program ) return TAB_ITEMS
+
+  if(program.uses_peer2peer
+      > 0){
+    TAB_ITEMS.push({
+      title: "peer_to_peer",
+      icon: "PeerToPeer",
+      to: "/participant/peer-to-peer",
+    },)
+  }
+
+  if(program.uses_leaderboards > 0){
+    TAB_ITEMS.push({
+      title: "leaderboards",
+      icon: "Leaderboards",
+      to: "/participant/leaderboards",
+    },)
+  }
+
+  if(program.uses_goal_tracker > 0){
+    TAB_ITEMS.push({
+      title: "my_goals",
+      icon: "MyGoals",
+      to: "/participant/my-goals",
+    },)
+  }
+
+  return TAB_ITEMS;
+
+}
