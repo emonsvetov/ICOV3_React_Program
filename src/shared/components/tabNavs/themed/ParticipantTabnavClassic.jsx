445b068c037cbc87f6c190e366a4e267b013a130
Arvind Kumar <arvind.mailto@gmail.com>
2023-08-17 11:48:58 +0530

    fixing message

--- src/shared/components/tabNavs/themed/ParticipantTabnavClassic.jsx
+++ src/shared/components/tabNavs/themed/ParticipantTabnavClassic.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Tabnav from "../components/Tabnav"; 
+import { getTabnavItems } from "../components/TabnavItems";
 
 const TabnavClassic = (props)=> {
   return <Tabnav {...props} />
@@ -11,59 +12,36 @@ const TabnavClassic = (props)=> {
 const ParticipantTabnavClassic = ({program, template}) => {
   const { t } = useTranslation();
 
-  const TAB_ITEMS = [
-    { title: "my_rewards", icon: "redeem", to: `/participant/my-points` },
-  ];
-
-  if(program.uses_peer2peer
-      > 0) {
-    TAB_ITEMS.push({
-      title: "peer_to_peer",
-      icon: "gift",
-      to: "/participant/peer-to-peer",
-    })
-  }
-
-  if(program.uses_leaderboards > 0){
-    TAB_ITEMS.push({
-      title: "leaderboards",
-      icon: "leaderboard",
-      to: "/participant/leaderboards",
-    })
-  }
-
-  if(program.uses_goal_tracker > 0){
-    TAB_ITEMS.push({
-      title: "my_goals",
-      icon: "survey",
-      to: "/participant/my-goals",
-    })
-  }
-
+  const TAB_ITEMS = getTabnavItems(program);
+  console.log(TAB_ITEMS)
   let navigate = useNavigate();
   if( ! program ) return 'loading...'
-
-  const theObj = {__html: template?.participant_homepage_message
-    ? template.participant_homepage_message : t("participant_homepage_message")};
-    
   return (
-      <div className="">
-        <ul className="horizontal d-flex justify-content-evenly">
-          {TAB_ITEMS.map((item, key) => {
-            return (
-                Object.keys(TAB_ITEMS).length > 1 ?
-                    <li key={key} onClick={() => navigate(item.to)}>
-                      <TabnavClassic title={t(item.title)} icon={item.icon}/>
-                    </li>
-                    :
-                    <h5 style={{color: 'black'}} className="social-wall-item-notification-body padding-10" dangerouslySetInnerHTML={theObj}></h5>
-            );
-          })}
-        </ul>
-      </div>
+    <div className="">
+    {
+      ( TAB_ITEMS.length < 1 ) &&
+      <h5 style={{color: 'black'}} className="social-wall-item-notification-body padding-10 pt-3">
+        {template?.participant_homepage_message
+        ? template.participant_homepage_message.replace(/(<([^>]+)>)/ig, "")
+        : t("participant_homepage_message")}
+      </h5>
+    }
+    { 
+      TAB_ITEMS.length > 0 &&
+      <ul className="horizontal d-flex justify-content-evenly">
+      {TAB_ITEMS.map((item, key) => {
+        return (
+          <li key={key} onClick={() => navigate(item.to)}>
+              <TabnavClassic title={t(item.title)} icon={item.icon}/>
+          </li> 
+        );
+      })}
+      </ul>
+    }
+  </div>
   );
 };
 export default connect((state) => ({
   program: state.program,
-  template: state.domain?.program?.template,
+  template: state.template,
 }))(ParticipantTabnavClassic);
--- src/shared/components/tabNavs/themed/ParticipantTabnavClear.jsx
+++ src/shared/components/tabNavs/themed/ParticipantTabnavClear.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import "../components/style.scss";
+import { getTabnavItems } from "../components/TabnavItems";
 
 function TabnavClear(props) {
   const { title, icon, isActive } = props;
@@ -30,58 +31,33 @@ TabnavClear.propTypes = {
 TabnavClear.defaultProps = {};
 
 export const ParticipantTabnavClear = ({program, template}) => {
-    console.log(template)
+    // console.log(template)
     const { t } = useTranslation();
-    const TAB_ITEMS = [
-      // { title: "my_rewards", icon: "MyRewards", to: `/participant/my-points` },
-    ];
-  
-    if(program.uses_peer2peer
-        > 0){
-      TAB_ITEMS.push({
-        title: "peer_to_peer",
-        icon: "PeerToPeer",
-        to: "/participant/peer-to-peer",
-      },)
-    }
-  
-    if(program.uses_leaderboards > 0){
-      TAB_ITEMS.push({
-        title: "leaderboards",
-        icon: "Leaderboards",
-        to: "/participant/leaderboards",
-      },)
-    }
-  
-    if(program.uses_goal_tracker > 0){
-      TAB_ITEMS.push({
-        title: "my_goals",
-        icon: "MyGoals",
-        to: "/participant/my-goals",
-      },)
-    }
-
-    console.log(TAB_ITEMS)
+    const TAB_ITEMS = getTabnavItems(program)
   
     let navigate = useNavigate();
     return (
       <div className="">
-        <ul className="horizontal d-flex justify-content-evenly">
+        {
+          ( TAB_ITEMS.length < 1 ) &&
+          <h5 style={{color: 'black'}} className="social-wall-item-notification-body padding-10 pt-3">
+            {template?.participant_homepage_message
+            ? template.participant_homepage_message.replace(/(<([^>]+)>)/ig, "")
+            : t("participant_homepage_message")}
+          </h5>
+        }
+        { 
+          TAB_ITEMS.length > 0 &&
+          <ul className="horizontal d-flex justify-content-evenly">
           {TAB_ITEMS.map((item, key) => {
             return (
-                Object.keys(TAB_ITEMS).length > 1 ?
-                    <li key={key} onClick={() => navigate(item.to)}>
-                        <TabnavClear title={t(item.title)} icon={item.icon}/>
-                    </li>
-                    :
-                    <h5 style={{color: 'black'}} className="social-wall-item-notification-body padding-10">
-                      {template?.participant_homepage_message
-                      ? template.participant_homepage_message.replace(/(<([^>]+)>)/ig, "")
-                      : t("participant_homepage_message")}
-                    </h5>
+              <li key={key} onClick={() => navigate(item.to)}>
+                  <TabnavClear title={t(item.title)} icon={item.icon}/>
+              </li> 
             );
           })}
-        </ul>
+          </ul>
+        }
       </div>
     );
 };
