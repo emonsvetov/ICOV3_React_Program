21a73e68ee70fd227d077e4610eb09d68474e1c8
Evgeny Monsvetov <emonsvetov@incento.com>
2023-08-31 13:28:41 -0500

    Participnat my codes page issue.

--- src/containers/Participant/GiftCodes/components/RedemptionToPrint.jsx
+++ src/containers/Participant/GiftCodes/components/RedemptionToPrint.jsx
@@ -10,20 +10,20 @@ export const RedemptionToPrint = React.forwardRef((props, ref) => {
         <table >
           <tr>
             <td style={{width: '150px', padding: '10px'}}>
-              <img style={{width: '100%'}} src={storageUrl + props.merchant.icon} className="gift-icon" />
+              <img style={{width: '100%'}} src={storageUrl + props.merchant?.icon} className="gift-icon" />
             </td>
             <td>
-              <div><h5>{props.merchant.name}</h5></div>
+              <div><h5>{props.merchant?.name}</h5></div>
               <div><h5>${parseFloat(props.sku_value).toFixed(2)}  Gift Code</h5></div>
               <div>&nbsp;</div>
               <div>Redeem your gift code by visiting the following URL:</div>
-              <a style={{color: 'blue'}} href={props.merchant.website} >{props.merchant.website}</a>
+              <a style={{color: 'blue'}} href={props.merchant?.website} >{props.merchant?.website}</a>
               <div>&nbsp;</div>
               <div><b>Gift Code: </b>{props.code}</div>
               <div><b>PIN: </b>{props.pin}</div>
               <div>&nbsp;</div>
               <div>Redemption Instructions</div>
-              <div className="right" dangerouslySetInnerHTML={createMarkup(props.merchant.redemption_instruction)}/>
+              <div className="right" dangerouslySetInnerHTML={createMarkup(props.merchant?.redemption_instruction)}/>
             </td>
           </tr>
         </table>
