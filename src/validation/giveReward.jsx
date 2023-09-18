d74e56cd35af2e87236be0a7cfc9837225bf125c
Evgeny Monsvetov <emonsvetov@incento.com>
2023-09-18 12:16:37 -0500

    ICOV3-42. Badge requires a point value, but should not - done.

--- src/validation/giveReward.jsx
+++ src/validation/giveReward.jsx
@@ -2,17 +2,15 @@ import { createFinalFormValidation } from "@lemoncode/fonk-final-form";
 import { Validators } from '@lemoncode/fonk';
 import { isNumber } from '@lemoncode/fonk-is-number-validator';
 import { isTrue } from '@lemoncode/fonk-is-true-validator';
-import {isObject} from "@/shared/helpers";
+import {isBadgeAward} from "@/shared/helpers";
+
 isTrue.setErrorMessage("This field is required");
 
 Validators.required.setErrorMessage("This field is required");
 
 export const overrideCashValueValidator = args => {
-  const { value, values } = args;
-
-  console.log(args);
-
-  if( values.event_type_id == 5 || (isObject(values.event_type_id) && values.event_type_id.value == 5)){
+  const { values } = args;
+  if( isBadgeAward(values.event_type_id) ){
       return {
           succeeded: true,
           type: 'override_cash_value_validator',
@@ -24,8 +22,8 @@ export const overrideCashValueValidator = args => {
 }
 
 export const awardingPointsRequiredValidator = args => {
-  const { value, values } = args;
-  if( values.event_type_id == 5 || (isObject(values.event_type_id) && values.event_type_id.value == 5)){
+  const { values } = args;
+  if( isBadgeAward(values.event_type_id) ){
       return {
           succeeded: true,
           type: 'awarding_points_required_validator',
@@ -37,8 +35,8 @@ export const awardingPointsRequiredValidator = args => {
 }
 
 export const awardingPointsNumberValidator = args => {
-  const { value, values } = args;
-  if( values.event_type_id == 5 || (isObject(values.event_type_id) && values.event_type_id.value == 5)){
+  const { values } = args;
+  if( isBadgeAward(values.event_type_id) ){
       return {
           succeeded: true,
           type: 'awarding_points_number_validator',
@@ -52,8 +50,8 @@ export const awardingPointsNumberValidator = args => {
 const validationSchema = {
     field: {
         event_id: [Validators.required.validator],
-        // override_cash_value: [Validators.required.validator],
-        // awarding_points: [Validators.required.validator, isNumber.validator],
+        override_cash_value: [overrideCashValueValidator],
+        awarding_points: [awardingPointsRequiredValidator, awardingPointsNumberValidator],
         message: [Validators.required.validator],
     }
 }
