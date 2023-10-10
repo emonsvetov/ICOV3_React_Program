498d927e5b0cd0df6ce1d2e8c4b7f4586e60be25
Oleg Ganshonkov <oganshonkov@incentco.com>
2023-10-10 13:39:00 +0300

    ICOV3-22. Add Reports. Award Detail - Done.

---
+++ src/containers/Manager/Report/AwardDetail/components/columns.jsx
@@ -0,0 +1,58 @@
+import {toCurrency, toPoints} from '@/shared/helpers'
+export const TABLE_COLUMNS = [
+  {
+    Header: "Event",
+    accessor: "event_name",
+  },
+  {
+    Header: "GL Code",
+    accessor: "ledger_code",
+  },
+  {
+    Header: "Date",
+    accessor: "posting_timestamp",
+  },
+  {
+    id: 'to',
+    Header: () => (<div style={{textAlign: 'center', borderTop: '1px solid #ccc', paddingTop: 6}}>To</div>),
+    className: 'align-center',
+    Footer: "",
+    columns: [
+      {
+        Header: "First Name",
+        accessor: "recipient_first_name",
+        width: 100
+      },
+      {
+        Header: "Last Name",
+        accessor: "recipient_last_name",
+        width: 100
+      },
+      {
+        Header: "Email",
+        accessor: "recipient_email",
+      }
+    ]
+  },
+  {
+    Header: "From",
+    accessor: data => (
+      data.awarder_first_name + " " + data.awarder_last_name
+    ),
+    width: 120
+  },
+  {
+    Header: "Referrer",
+    accessor: "referrer",
+  },
+  {
+    Header: "Notes",
+    accessor: "notes",
+  },
+  {
+    Header: "Dollar Value",
+    accessor: "dollar_value",
+    Cell: ({ row, value }) => { return toCurrency(value); },
+    width: 110
+  },
+];
---
+++ src/containers/Manager/Report/AwardDetail/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Row} from 'reactstrap';
+import AwardDetailCard from './components/AwardDetailIndex.jsx';
+
+const AwardDetail = () => {
+  return (
+      <Row>
+        <AwardDetailCard/>
+      </Row>
+  )
+}
+
+export default AwardDetail;
