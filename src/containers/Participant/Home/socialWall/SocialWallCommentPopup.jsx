4e7d1d698091c0827528bc2e27252aa4da3f46f9
ashwani kumar <ashwanikumar@inimist.com>
2023-10-30 09:35:47 +0530

    changes task ICOV3-63

--- src/containers/Participant/Home/socialWall/SocialWallCommentPopup.jsx
+++ src/containers/Participant/Home/socialWall/SocialWallCommentPopup.jsx
@@ -1,7 +1,7 @@
 //SystemModules
-import React, { useState} from 'react';
+import React, { useState, useEffect } from "react";
 import { connect, useDispatch } from "react-redux";
-import {Form, Field} from 'react-final-form';
+import { Form, Field } from "react-final-form";
 import {
   Modal,
   Col,
@@ -12,123 +12,185 @@ import {
   Card,
   CardHeader,
   CardBody,
-} from 'reactstrap';
+} from "reactstrap";
 
 //CustomModules
 
-import {createSocialWallPost, setSocialWallPostType} from '@/redux/actions/socialWallPostActions';
-import {getSocialWallPostType} from '@/services/program/getSocialWallPostTypes'
-import {getSocialWallPosts} from '@/services/program/getSocialWallPosts'
-import Editor from '@/shared/components/form/Editor';
-
+import {
+  createSocialWallPost,
+  setSocialWallPostType,
+} from "@/redux/actions/socialWallPostActions";
+import { getSocialWallPostType } from "@/services/program/getSocialWallPostTypes";
+import { getSocialWallPosts } from "@/services/program/getSocialWallPosts";
+import Editor from "@/shared/components/form/Editor";
+import { getUsers } from "@/services/program/getUsers";
+import { SignalCellularNullSharp } from "@material-ui/icons";
 //DefaultComponent
 
-const SocialWallCommentPopup = ({isOpen, setOpen, toggle, socialWallPost, program, organization, auth, setSocialWallPosts, swpGlobal, postType = 'comment', setPostType}) => {
-
+const SocialWallCommentPopup = ({
+  isOpen,
+  setOpen,
+  toggle,
+  socialWallPost,
+  program,
+  organization,
+  auth,
+  setSocialWallPosts,
+  swpGlobal,
+  postType = "comment",
+  setPostType,
+}) => {
   // console.log("SocialWallCommentPopup")
   // console.log(`isOpen: ${isOpen}`)
-  if( swpGlobal && swpGlobal?.newPostType ) {
-    postType = swpGlobal.newPostType
+  if (swpGlobal && swpGlobal?.newPostType) {
+    postType = swpGlobal.newPostType;
   }
 
-  const dispatch = useDispatch()
-  const [value, setValue] = useState('')
-  const onSubmit = values => {
-
+  const dispatch = useDispatch();
+  const [value, setValue] = useState("");
+  const [mentionsUserIds, setMentionsUserIds]=useState(null)
+  const [usersMentionData, setUsersMentionData] = useState([]);
+  const onSubmit = (values) => {
     // console.log("SocialWallCommentPopup > onSubmit")
     // console.log(program.uses_social_wall)
     // console.log(organization.id)
 
     if (program.uses_social_wall) {
-
       let socialWallPostData = {
-        'comment': value,
-        'program_id': program.id,
-        'organization_id': organization.id,
-        'event_xml_data_id': null,
-        'awarder_program_id': null,
-        'like':null,
-        'likesCount': 0,
-        'social_wall_post_id': null,
-        'sender_user_account_holder_id': null,
-        'receiver_user_account_holder_id': null,
-        'social_wall_post_type_id': null,
+        comment: value,
+        program_id: program.id,
+        organization_id: organization.id,
+        event_xml_data_id: null,
+        awarder_program_id: null,
+        like: null,
+        likesCount: 0,
+        social_wall_post_id: null,
+        sender_user_account_holder_id: null,
+        receiver_user_account_holder_id: null,
+        social_wall_post_type_id: null,
+        mentions_user_ids:mentionsUserIds
+      };
+      if (socialWallPost?.id) {
+        socialWallPostData.social_wall_post_id = socialWallPost.id;
       }
 
-      if( socialWallPost?.id )  {
-        socialWallPostData.social_wall_post_id = socialWallPost.id
+      if (auth?.account_holder_id) {
+        socialWallPostData.sender_user_account_holder_id =
+          auth.account_holder_id;
       }
 
-      if( auth?.account_holder_id )  {
-        socialWallPostData.sender_user_account_holder_id = auth.account_holder_id
+      if (socialWallPost?.receiver_user_account_holder_id) {
+        socialWallPostData.receiver_user_account_holder_id =
+          socialWallPost.receiver_user_account_holder_id;
       }
 
-      if( socialWallPost?.receiver_user_account_holder_id )  {
-        socialWallPostData.receiver_user_account_holder_id = socialWallPost.receiver_user_account_holder_id
+      if (
+        !socialWallPostData.receiver_user_account_holder_id &&
+        auth?.account_holder_id
+      ) {
+        socialWallPostData.receiver_user_account_holder_id =
+          auth.account_holder_id;
       }
 
-      if( !socialWallPostData.receiver_user_account_holder_id && auth?.account_holder_id ) {
-        socialWallPostData.receiver_user_account_holder_id = auth.account_holder_id
-      }
-
-      getSocialWallPostType(organization.id, program.id, postType)
-        .then(socialWallPostType => {
+      getSocialWallPostType(organization.id, program.id, postType).then(
+        (socialWallPostType) => {
           // console.log(socialWallPostType)
           // setPostType(null)
           // return
-          socialWallPostData.social_wall_post_type_id = socialWallPostType.id
-          dispatch(createSocialWallPost(organization.id, program.id, socialWallPostData))
+          socialWallPostData.social_wall_post_type_id = socialWallPostType.id;
+          dispatch(
+            createSocialWallPost(
+              organization.id,
+              program.id,
+              socialWallPostData,
+
+            )
+          )
             .then((res) => {
-              setPostType(null)
-              toggle()
+              setPostType(null);
+              toggle();
               getSocialWallPosts(organization.id, program.id, 0, 999999)
-                .then(data => {
+                .then((data) => {
                   // console.log(data)
                   setSocialWallPosts(data);
                 })
-                .catch(error => {
+                .catch((error) => {
                   console.log(error.response.data);
-                })
+                });
             })
-            .catch(err => {
-              console.log(err)
-            })
-        })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      );
     }
   };
 
+  useEffect(() => {
+    let mentionsData = [];
+    getUsers(organization.id, program.id)
+      .then((users) => {
+        const username = users.results?.map((user) => {
+          const newObject = {
+            id: user.id,
+            name: user.first_name,
+            fullName: user.name,
+          };
+          mentionsData.push(newObject);
+        });
+        setUsersMentionData(mentionsData);
+      })
+      .catch((err) => {
+        console.log("error in fetch  users", err);
+      });
+  },[getUsers]);
+
   return (
-    <Modal className={`modal-2col modal-lg`} isOpen={isOpen} toggle={() => setOpen(true)}>
-
-      <Card className='w-100'>
+    <Modal
+      className={`modal-2col modal-lg`}
+      isOpen={isOpen}
+      toggle={() => setOpen(true)}
+    >
+      <Card className="w-100">
         <CardHeader tag="h3">
-
           Add a comment
-
-          <Button className='btn btn-lg float-end' close onClick={toggle}/>
-
+          <Button className="btn btn-lg float-end" close onClick={toggle} />
         </CardHeader>
         <CardBody>
-          <Form
-            onSubmit={onSubmit}
-            initialValues={{}}
-          >
-            {({handleSubmit, form, submitting, pristine, values}) => (
-              <form className="form flex-column justify-content-evenly" onSubmit={handleSubmit}>
+          <Form onSubmit={onSubmit} initialValues={{}}>
+            {({ handleSubmit, form, submitting, pristine, values }) => (
+              <form
+                className="form flex-column justify-content-evenly"
+                onSubmit={handleSubmit}
+              >
                 <Row>
                   <Col md="12">
                     <Field name="message">
-                      {({input, meta}) => (
+                      {({ input, meta }) => (
                         <FormGroup>
-                          <Editor setValue={setValue} placeholder="" />
-                          {meta.touched && meta.error && <FormFeedback> {meta.error}</FormFeedback>}
+                       {/* {usersMentionData.length !== 0 ? ( */}
+                            <Editor
+                              setValue={setValue}
+                              placeholder=""
+                              setMentionsUserIds={setMentionsUserIds}
+                              usersMentionData={usersMentionData}
+                              
+                            />
+                           {/* ) : (
+                            "Loading"
+                          )}  */}
+                          {meta.touched && meta.error && (
+                            <FormFeedback> {meta.error}</FormFeedback>
+                          )}
                         </FormGroup>
                       )}
                     </Field>
                   </Col>
                 </Row>
-                <div className='d-flex justify-content-end'>
-                  <Button color='danger' type='submit'>Post</Button>
+                <div className="d-flex justify-content-end">
+                  <Button color="danger" type="submit">
+                    Post
+                  </Button>
                 </div>
               </form>
             )}
@@ -136,14 +198,17 @@ const SocialWallCommentPopup = ({isOpen, setOpen, toggle, socialWallPost, progra
         </CardBody>
       </Card>
     </Modal>
-  )
-}
+  );
+};
 const mapStateToProps = (state) => {
   return {
-    swpGlobal: state.socialWallPost
+    swpGlobal: state.socialWallPost,
   };
 };
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   setPostType: (type) => dispatch(setSocialWallPostType(type)),
 });
-export default connect(mapStateToProps, mapDispatchToProps)(SocialWallCommentPopup);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SocialWallCommentPopup);
--- src/shared/components/form/Editor.jsx
+++ src/shared/components/form/Editor.jsx
@@ -1,30 +1,72 @@
-import React from 'react'
-import {CKEditor} from 'ckeditor4-react';
-
+import React from "react";
+import { CKEditor } from "ckeditor4-react";
 
 const Editor = (props) => {
-  const {setValue, placeholder} = props;
+  const { setValue, placeholder, usersMentionData, setMentionsUserIds } = props;
+
+  const mentionFeed = (query, callback) => {
+    let searchQuery = query.query.toLowerCase();
+    let data = usersMentionData.filter((item) => {
+      return item.name.toLowerCase().includes(searchQuery);
+    });
+    callback(data);
+  };
+
+  function getMentionsUsers(mentionsUsersContent) {
+    const checkMentionRegex = /@(\w+)/g;
+    const usersContentMentions = [];
+    let contentMatch;
+    while ((contentMatch = checkMentionRegex.exec(mentionsUsersContent)) !== null) {
+      usersContentMentions.push(contentMatch[0]);
+    }
+    const mentionUserIds = usersContentMentions?.map((mention) => {
+      // Use regular expression to extract the numeric part
+      const usersMatch = mention.match(/\d+/);
+      if (usersMatch) {
+        return usersMatch[0];
+      } else {
+        return null; // Handle mentions without a numeric part (no user ID)
+      }
+    });
+    const allMentionsUserIds = mentionUserIds?.map((userIds) => parseInt(userIds, 10));
+    setMentionsUserIds(allMentionsUserIds);
+  }
+
   return (
     <div>
-      <CKEditor
-        initData={placeholder}
-        config={{
-          toolbar: [
-            ['EmojiPanel'],
-          ],
-          extraPlugins: 'emoji',
-          removePlugins: 'image',
-        }}
-        onInstanceReady={() => {
-          // console.log('Editor is ready!');
-        }}
-        onChange={(event) => {
-          const data = event.editor.getData();
-          // console.log(data);
-          setValue(data);
-        }}
-      />
+      {usersMentionData.length !== 0 ? (
+        <CKEditor
+          initData={placeholder}
+          config={{
+            toolbar: [["EmojiPanel"]],
+            extraPlugins: "emoji,mentions,link",
+            removePlugins: "image",
+            mentions: [
+              {
+                feed: mentionFeed,
+                itemTemplate:
+                  '<li data-id="{id}">' +
+                  '<strong class="name">{name}</strong>' +
+                  "</li>",
+                outputTemplate: "<span>{name}{id}</span><span>&nbsp;</span>",
+                minChars: 0,
+              },
+            ],
+          }}
+          onInstanceReady={({ editor }) => {
+            // Handles native `instanceReady` event.
+          }}
+          onChange={(event) => {
+            const content = event.editor.getData();
+            getMentionsUsers(content);
+            setValue(content);
+          }}
+          name="editor1"
+        />
+      ) : (
+        "loading"
+      )}
     </div>
-  )
-}
-export default Editor
+  );
+};
+export default Editor;
