6f593cabea5b425e8762579b7ea274fb50f5d3fd
Vicky0603 <victoria.kovalyova14@gmail.com>
2024-02-05 21:45:06 -0300

    ICOV3-201:Alphabetize A - Z according to merchant name

--- src/containers/Participant/Home/index.jsx
+++ src/containers/Participant/Home/index.jsx
@@ -11,122 +11,122 @@ import { useTranslation } from "react-i18next";
 // import { setMerchant } from "@/redux/actions/merchantActions";
 
 const Home = ({ auth, program, template }) => {
-  const { t } = useTranslation();
-  // let props = {
-  //   organization,
-  //   program,
-  // };
-  let [showSocialWall, setShowSocialWall] = useState(null);
+    const { t } = useTranslation();
+    // let props = {
+    //   organization,
+    //   program,
+    // };
+    let [showSocialWall, setShowSocialWall] = useState(null);
 
-  const participant_homepage_message  = {__html: template?.participant_homepage_message
-    ? template.participant_homepage_message : t("participant_homepage_message")}
- 
+    const participant_homepage_message  = {__html: template?.participant_homepage_message
+            ? template.participant_homepage_message : t("participant_homepage_message")}
+
     useEffect(() => {
-    let result = false;
-    if (auth && program) {
-      if (
-        auth.user_status_id === USER_STATUS_PENDING_DEACTIVATION &&
-        program.remove_social_from_pending_deactivation
-      ) {
-        result = false;
-      } else if (program.uses_social_wall > 0) {
-        result = true;
-      }
-    }
-    setShowSocialWall(result);
-  }, [auth, program]);
+        let result = false;
+        if (auth && program) {
+            if (
+                auth.user_status_id === USER_STATUS_PENDING_DEACTIVATION &&
+                program.remove_social_from_pending_deactivation
+            ) {
+                result = false;
+            } else if (program.uses_social_wall > 0) {
+                result = true;
+            }
+        }
+        setShowSocialWall(result);
+    }, [auth, program]);
 
-  if (!auth || !program || !template) return t("loading");
+    if (!auth || !program || !template) return t("loading");
 
-  const HomeClear = () => {
-    const IMG_BACK = template.hero_banner
-      ? `${process.env.REACT_APP_API_STORAGE_URL}/${template.hero_banner}`
-      : `${process.env.PUBLIC_URL}/theme/clear/img/back.jpg`;
-    return (
-      <>
-        <div className="text-center mt-5 mb-5" dangerouslySetInnerHTML={participant_homepage_message}></div>
-        <div className="mainboard">
-          <div className="homeImgWrap">
-            <img src={IMG_BACK} alt="home_Img" />
-          </div>
-          <SlideOutMenu isFixed={false} />
-        </div>
-        <Container className={`${template.name} mt-5`} fluid>
-          <Row>
-            <Col md={3}>
-              <Points />
-            </Col>
-            <Col md={9}>
-              <ParticipantTabNavs program={program} />
-            </Col>
-          </Row>
-        </Container>
-        <Container className="">
-          {showSocialWall && (
-              <>
-                <SocialWallPanel />
-              </>
-          )}
-        </Container>
-        <div className="mt-5">
-          <h6 className="m-3">
-            {t("select_a_merchant_to_redeem_your_points")}
-          </h6>
-          <MerchantSlider />
-        </div>
-      </>
-    );
-  };
-  const HomeClassic = () => {
-    const IMG_BACK = template.hero_banner
-      ? `${process.env.REACT_APP_API_STORAGE_URL}/${template.hero_banner}`
-      : `${process.env.PUBLIC_URL}/theme/classic/img/back.png`;
-    return (
-      <>
-        <div className="text-center mt-5 mb-5" dangerouslySetInnerHTML={participant_homepage_message}></div>
-        <div className={"mainboard d-flex"}>
-          <SlideOutMenu isFixed={true} />
-          <div className="homeImgWrap">
-              <img src={IMG_BACK} alt="home_mg"/>
-          </div>
-        </div>
-        <Container>
-          <ParticipantTabNavs program={program} />
-        </Container>
-        <Container>
-          <Row>
-            <Col md={9}>
-              <div className="dashboard">
-                {showSocialWall && (
-                  <>
-                    <SocialWallPanel />
-                  </>
-                )}
-                <div className="mt-5">
-                  <h3>{t("select_a_merchant_to_redeem_your_points")}</h3>
-                  <MerchantSlider />
+    const HomeClear = () => {
+        const IMG_BACK = template.hero_banner
+            ? `${process.env.REACT_APP_API_STORAGE_URL}/${template.hero_banner}`
+            : `${process.env.PUBLIC_URL}/theme/clear/img/back.jpg`;
+        return (
+            <>
+                <div className="text-center mt-5 mb-5" dangerouslySetInnerHTML={participant_homepage_message}></div>
+                <div className="mainboard">
+                    <div className="homeImgWrap">
+                        <img src={IMG_BACK} alt="home_Img" />
+                    </div>
+                    <SlideOutMenu isFixed={false} />
                 </div>
-              </div>
-            </Col>
-            <Col md={3} className="mt-2">
-              <Points />
-            </Col>
-          </Row>
-        </Container>
-      </>
+                <Container className={`${template.name} mt-5`} fluid>
+                    <Row>
+                        <Col md={3}>
+                            <Points />
+                        </Col>
+                        <Col md={9}>
+                            <ParticipantTabNavs program={program} />
+                        </Col>
+                    </Row>
+                </Container>
+                <Container className="">
+                    {showSocialWall && (
+                        <>
+                            <SocialWallPanel />
+                        </>
+                    )}
+                </Container>
+                <div className="mt-5">
+                    <h6 className="m-3">
+                        {t("select_a_merchant_to_redeem_your_points")}
+                    </h6>
+                    <MerchantSlider />
+                </div>
+            </>
+        );
+    };
+    const HomeClassic = () => {
+        const IMG_BACK = template.hero_banner
+            ? `${process.env.REACT_APP_API_STORAGE_URL}/${template.hero_banner}`
+            : `${process.env.PUBLIC_URL}/theme/classic/img/back.png`;
+        return (
+            <>
+                <div className="text-center mt-5 mb-5" dangerouslySetInnerHTML={participant_homepage_message}></div>
+                <div className={"mainboard d-flex"}>
+                    <SlideOutMenu isFixed={true} />
+                    <div className="homeImgWrap">
+                        <img src={IMG_BACK} alt="home_mg"/>
+                    </div>
+                </div>
+                <Container>
+                    <ParticipantTabNavs program={program} />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col md={9}>
+                            <div className="dashboard">
+                                {showSocialWall && (
+                                    <>
+                                        <SocialWallPanel />
+                                    </>
+                                )}
+                                <div className="mt-5">
+                                    <h3>{t("select_a_merchant_to_redeem_your_points")}</h3>
+                                    <MerchantSlider />
+                                </div>
+                            </div>
+                        </Col>
+                        <Col md={3} className="mt-2">
+                            <Points />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    };
+    return (
+        (template?.name === "clear" && <HomeClear />) ||
+        (template?.name === "classic" && <HomeClassic />)
     );
-  };
-  return (
-    (template?.name === "clear" && <HomeClear />) ||
-    (template?.name === "classic" && <HomeClassic />)
-  );
 };
 
 const mapStateToProps = (state) => {
-  return {
-    auth: state.auth,
-    program: state.program,
-    template: state.template,
-  };
+    return {
+        auth: state.auth,
+        program: state.program,
+        template: state.template,
+    };
 };
 export default connect(mapStateToProps)(Home);
