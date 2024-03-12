71f73fe4e8256f5abcf9a88133f4be44dbc9eaff
cryptofan430 <panda.kk1114@gmail.com>
2024-03-12 00:53:10 -0400

    Change text as rewards events

--- src/containers/Manager/ProgramSettings/index.jsx
+++ src/containers/Manager/ProgramSettings/index.jsx
@@ -79,7 +79,7 @@ const ProgramSettings = ({ auth, program, organization }) => {
       <Container>
         <Row className="mt-4">
           <Col md={10}>
-            <h3>Program Settings</h3>
+            <h3>Rewards Events</h3>
             <span>
               Customize your settings for your own unique program. You can
               create and/or select reward events and insert the default reward
