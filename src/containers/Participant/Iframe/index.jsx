6cc023f7b00178de2a0b0c43074467e8c192090e
cryptofan430 <panda.kk1114@gmail.com>
2024-03-14 14:19:52 -0400

    Fix iframe on page & menu

--- src/containers/Participant/Iframe/index.jsx
+++ src/containers/Participant/Iframe/index.jsx
@@ -1,35 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { connect } from "react-redux";
-import { PDF } from "@/containers/Participant/Training/components/PDF";
 import { Col, Row } from "reactstrap";
 import Sidebar from "@/containers/Layout/sidebar";
 import { useTranslation } from "react-i18next";
 import axios from 'axios'
-import {MEDIA_TYPES} from "@/containers/LogIn/components/LogInForm";
 
 const Iframe = ({ template, auth, program, organization }) => {
-  const [iframe, setIframe] = useState([]);
-  const { t } = useTranslation();
+  const [iframe, setIframe] = useState({});
 
   let params = useParams();
+  const getData = async ({categoryId}) => {
+    const url = axios.defaults.baseURL + `/organization/${organization?.id}/program/${program?.id}/digital-media-type/${categoryId}`;
+    try {
+      const response = await axios.get(url);
+      if(response.data.length){
+        setIframe(response.data[0]);
+      }
+    } catch (e) {
+      throw new Error(`API error:${e?.message}`);
+    }
+  }
 
-//   const getData = async ({categoryId}) => {
-//     const url = axios.defaults.baseURL + `/organization/${organization.id}/program/${program.id}/iframe/${categoryId}`;
-//     try {
-//       const response = await axios.get(url);
-//       setIframe(response.data);
-//       return response.data;
-//     } catch (e) {
-//       throw new Error(`API error:${e?.message}`);
-//     }
-//   }
+  useEffect(() => {
+    if(program && organization)
+      getData(params);
+  }, [program, organization, params]);
 
-//   useEffect(() => {
-//     getData(params);
-//   }, [params]);
-  const mediaTypes = JSON.parse(localStorage.getItem(MEDIA_TYPES)) || [];
-  const iframeLink = mediaTypes.filter(item =>item.program_media_type_id == params.categoryId);
   return (
     <Row className="mt-4">
       <Col md={4}>
@@ -40,7 +37,7 @@ const Iframe = ({ template, auth, program, organization }) => {
         <div className="pdf-link">
           <h2 className="text-uppercase text-center mb-5"></h2>
           <div className="d-flex flex-row mb-3 flex-wrap mt-4 align-items-center" >
-            <iframe src={iframeLink[0].menu_link} className="w-100" style={{height: '500px'}}/>
+            <iframe src={iframe?.menu_link} className="w-100" style={{height: '500px'}}/>
           </div>
         </div>
       </Col>
