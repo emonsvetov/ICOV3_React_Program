95e2c11b5459ef8cd7053bd190a07adf62c40cd2
Arvind Kumar <arvind.mailto@gmail.com>
2024-03-18 10:04:11 +0530

    Unit number updates to manager fields

--- src/containers/Manager/InviteParticipant/index.jsx
+++ src/containers/Manager/InviteParticipant/index.jsx
@@ -5,9 +5,14 @@ import { Form, Field } from "react-final-form";
 import axios from "axios";
 import { connect } from "react-redux";
 import { useDispatch, flashSuccess, flash422 } from "@/shared/components/flash";
-import { makeLabelizedOptionsFromTree } from "@/shared/helpers";
+import {
+  makeLabelizedOptionsFromTree,
+  labelizeNamedData,
+  unpatchSelect
+} from "@/shared/helpers";
 import { getProgramTree } from "@/services/program/getProgramTree";
 import TemplateButton from "@/shared/components/TemplateButton";
+import getUnitNumbers from "@/services/program/getUnitNumbers";
 
 const InviteParticipant = ({ auth, organization, rootProgram }) => {
   const [programOptions, setProgramOptions] = useState([]);
@@ -25,6 +30,7 @@ const InviteParticipant = ({ auth, organization, rootProgram }) => {
   }*/
   let [program, setProgram] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [unitNumberOptions, setUnitNumberOptions] = useState(null);
   const dispatch = useDispatch();
   const onSelectProgram = (selectedOption) => {
     setProgram(selectedOption);
@@ -32,8 +38,25 @@ const InviteParticipant = ({ auth, organization, rootProgram }) => {
 
   const awardLevelOptions = [{ label: "Default", value: "default" }];
 
+  useEffect(() => {
+    let currentProgramId = program?.value ?? rootProgram?.id
+    if( currentProgramId ) {
+      getUnitNumbers(organization.id, currentProgramId, "assignable=1")
+      .then( res => {
+        setUnitNumberOptions(labelizeNamedData(res));
+      })
+    }
+  }, [organization, rootProgram, program]);
+
+  const onChangeProgramValue = ([fieldName, fieldVal], state, {changeValue }) => {
+    changeValue(state, 'unit_number', () => null);
+  }
+
   const onSubmit = (values) => {
     setLoading(true);
+    if( typeof values.unit_number === 'object') {
+      values = unpatchSelect(values, ['unit_number'])
+    }
     axios
       .put(
         `/organization/${organization.id}/program/${values.program.value}/invite`,
@@ -60,7 +83,11 @@ const InviteParticipant = ({ auth, organization, rootProgram }) => {
   return (
     <div className="invite-participant">
       <h2 className="title mb-3">Invite Participant</h2>
-      <Form onSubmit={onSubmit} initialValues={{}} validate={validate}>
+      <Form onSubmit={onSubmit} initialValues={{}} validate={validate}
+        mutators={{
+          onChangeProgramValue
+        }}
+      >
         {({ handleSubmit, form, submitting, pristine, values }) => (
           <form
             className="form d-flex flex-column justify-content-evenly"
@@ -89,6 +116,7 @@ const InviteParticipant = ({ auth, organization, rootProgram }) => {
                             placeholder={" --- "}
                             classNamePrefix="react-select"
                             value={program}
+                            onInputChange={form.mutators.onChangeProgramValue}
                             {...input}
                           />
                           {meta.touched && meta.error && (
@@ -189,6 +217,32 @@ const InviteParticipant = ({ auth, organization, rootProgram }) => {
                 </Field>
               </Col>
             </Row>
+            {unitNumberOptions && unitNumberOptions.length >0 && (
+              <Row>
+                <Col md="12">
+                  <Field name="unit_number">
+                    {({ input, meta }) => (
+                      <FormGroup>
+                        <Select
+                          options={unitNumberOptions}
+                          clearable={true}
+                          className="react-select"
+                          placeholder={"Unit Number"}
+                          classNamePrefix="react-select"
+                          parse={(value) => {
+                            return value;
+                          }}
+                          {...input}
+                        />
+                        {meta.touched && meta.error && (
+                          <span className="form-error">{meta.error}</span>
+                        )}
+                      </FormGroup>
+                    )}
+                  </Field>
+                </Col>
+              </Row>
+            )}
             <div className="d-flex justify-content-center">
               <TemplateButton
                 type="submit"
